5bdda514da6783d0e412a5f1b399f9686f3a423c
Brittany Storoz <brittanystoroz@gmail.com>
2016-10-04 12:33:08 -0600

    Boilerplate for mod 3 app

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
+import reducer from './reducers'
+import App from './containers/App'
+
+const middleware = [ thunk, createLogger ]
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware)
+)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
