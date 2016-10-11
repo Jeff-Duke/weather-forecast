cd412dbe6e90c3665a5810b4fcb2653bc7269c87
mjvalade <mjvalade@gmail.com>
2016-10-11 12:17:22 -0600

    a few passing tests

--- src/components/CityCard.test.js
+++ src/components/CityCard.test.js
@@ -4,13 +4,13 @@ import CityCard from './CityCard';
 
 describe('CityCard component', () => {
   it('renders the name of the city in <h1> tags', () => {
-    const wrapper = shallow( <CityCard /> );
+    const wrapper = shallow( <CityCard pinnedCurrentForecast={{}}/> );
 
     expect(wrapper.contains('City Name')).toEqual(true);
   });
 
   it('should have a Link to route to the extended forecast', () => {
-    const wrapper = shallow( <CityCard /> );
+    const wrapper = shallow( <CityCard pinnedCurrentForecast={{}}/> );
 
     expect(wrapper.find('.pinned-forecast-extended').length).toEqual(1);
   });
