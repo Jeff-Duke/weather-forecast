6c37a231e357c696f47c30bdce9699a757d8d429
mjvalade <mjvalade@gmail.com>
2016-10-11 11:47:11 -0600

    set up testing environments

--- src/reducers/local-weather.js
+++ src/reducers/local-weather.js
@@ -1,12 +1,12 @@
 const localWeatherReducer = (state = {}, action) => {
-    switch (action.type) {
+  switch (action.type) {
 
-        case 'CURRENT_LOCAL_WEATHER_GPS':
-            return Object.assign({}, state, action.currentLocalForecast);
+    case 'CURRENT_LOCAL_WEATHER_GPS':
+      return Object.assign({}, state, action.currentLocalForecast);
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 };
 
 export default localWeatherReducer;
---
+++ src/test/reducers/reducers.test.js
@@ -0,0 +1,12 @@
+import * as reducers from '../../reducers';
+
+
+import { receiveCurrentWeatherByGPS } from '../../actions';
+
+describe('currentWeatherByZipReducer', () => {
+  it('should handle CURRENT_LOCAL_WEATHER_ZIP', () => {
+    expect(true).toEqual(true)
+  });
+
+
+});
